a6a23e69cb271750183f8a7667a81743c65b6dcc
lyndontavares <lyndon.tavares@datapar.com.py>
2021-01-11 18:36:51 -0300

    atualizado

--- learn-react/app7/src/index.js
+++ learn-react/app7/src/index.js
@@ -13,3 +13,30 @@ ReactDOM.render(
     document.getElementById('root'),
 
  )
+
+/*
+   Retornando 2 elementos Adjacentes
+
+   Ténica.1:
+   ReactDOM.render(
+
+    <>
+      <h1> Olá Mundo!</h1>
+     <Primeiro/>
+    </>,
+
+    document.getElementById('root'),
+
+ )
+
+  Ténica.2:
+  ReactDOM.render(
+
+    <React.Fragment>
+      <h1> Olá Mundo!</h1>
+     <Primeiro/>
+    </React.Fragment>,
+
+    document.getElementById('root'),
+
+ ) */
